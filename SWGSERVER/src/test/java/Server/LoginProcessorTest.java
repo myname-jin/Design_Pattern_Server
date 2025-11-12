4eb7d49aa1558aff3b784941882d0c004e3ee827
myname-jin <adsd3@example.com>
2025-11-12 16:45:58 +0900

    add : 소켓

--- SWGSERVER/src/test/java/Server/LoginProcessorTest.java
+++ SWGSERVER/src/test/java/Server/LoginProcessorTest.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
-package Server;
+/*package Server;
 
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -54,4 +54,4 @@ public class LoginProcessorTest {
         boolean result = processor.validateLogin("admin01", "wrongpw", "admin");
         assertFalse(result);
     }
-}
+}*/
